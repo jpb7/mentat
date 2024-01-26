1e126ed1a476b29b4fdc81dcab54dc9bfab99059
Jacob Bentley <jpb7@pdx.edu>
2024-01-26 06:36:19 -0800

    Add initial stylesheet and apply to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './App.module.css';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Under Construction</h1>
     </div>
   );
