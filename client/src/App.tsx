cdb3122116eaadd38022cf07525bbd21cbddca7e
Jacob Bentley <jpb7@pdx.edu>
2024-01-27 03:35:01 -0800

    MNT-7: create MainMenu component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MainMenu from './components/MainMenu';
 import styles from './App.module.css';
 
 const App: React.FC = () => {
   return (
     <div className={styles.container}>
-      <h1>Under Construction</h1>
+      <MainMenu />
     </div>
   );
 };
---
+++ client/src/components/MainMenu.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './MainMenu.module.css';
+
+const MainMenu: React.FC = () => {
+  return (
+    <div className={styles.menu}>
+      <button className={styles.menuItem}>exercise 1</button>
+      <button className={styles.menuItem}>exercise 2</button>
+      <button className={styles.menuItem}>exercise 3</button>
+    </div>
+  );
+};
+
+export default MainMenu;
