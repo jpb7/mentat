20e642484a0fd54a31669b903d7fd64950caf2ec
Jacob Bentley <jpb7@pdx.edu>
2024-01-31 00:57:46 -0800

    Add DrillButton component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import DrillBox from './components/DrillBox';
+import DrillButton from './components/DrillButton';
 import MainMenu from './components/MainMenu';
 import TitleBar from './components/TitleBar';
 import styles from './App.module.css';
@@ -12,12 +13,29 @@ const App: React.FC = () => {
 				<Link to='/' className={styles.titleLink}>
 					<TitleBar title='mentat' />
 				</Link>
+
 				<Routes>
 					<Route path='/' element={<MainMenu />} />
-					<Route path='/exercise1' element={<DrillBox drillId='exercise1' />} />
-					<Route path='/exercise2' element={<DrillBox drillId='exercise2' />} />
-					<Route path='/exercise3' element={<DrillBox drillId='exercise3' />} />
+					<Route path='/exercise1' element={
+						<>
+							<DrillBox drillId='exercise1' />
+							<DrillButton />
+						</>
+					} />
+					<Route path='/exercise2' element={
+						<>
+							<DrillBox drillId='exercise2' />
+							<DrillButton />
+						</>
+					} />
+					<Route path='/exercise3' element={
+						<>
+							<DrillBox drillId='exercise3' />
+							<DrillButton />
+						</>
+					} />
 				</Routes>
+
 			</div>
 		</Router>
   );
---
+++ client/src/components/DrillButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './DrillButton.module.css';
+
+const DrillButton: React.FC = () => {
+	return(
+		<button className={styles.drillButton}>
+			show answer
+		</button>
+	);
+}
+
+export default DrillButton;
+
