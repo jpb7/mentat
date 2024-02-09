c70e61864f5ac3c0d031bdbc1ecbdd925e16cbb7
Jacob Bentley <jpb7@pdx.edu>
2024-02-08 21:59:52 -0800

    Resize DrillBox and retab

--- client/src/components/DrillBox.tsx
+++ client/src/components/DrillBox.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import styles from './DrillBox.module.css';
 
 type DrillBoxProps = {
-	drillId: string;
+  drillId: string;
 }
 
 const DrillBox: React.FC<DrillBoxProps> = ({ drillId }) => {
-	return (
-		<div className={styles.drillBox}>
-			<p>{drillId}</p>
-		</div>
-	);
+  return (
+    <div className={styles.drillBox}>
+      <p>{drillId}</p>
+    </div>
+  );
 };
 
 export default DrillBox;
